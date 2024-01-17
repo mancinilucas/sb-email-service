428b29492ec34b95bd186b4027660546e0987ded
mancinilucas <mancini.lucasg@gmail.com>
2024-01-17 19:53:43 -0300

    created emailsenderservice base class

---
+++ src/main/java/com/mancinilucas/emailservice/application/EmailSenderService.java
@@ -0,0 +1,19 @@
+package com.mancinilucas.emailservice.application;
+
+import com.mancinilucas.emailservice.core.EmailSenderUseCase;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class EmailSenderService implements EmailSenderUseCase {
+
+    private final EmailSenderGateway emailSenderGateway;
+
+    @Autowired
+    public EmailSenderService(EmailSenderGateway emailGateway){
+        this.emailSenderGateway = emailGateway;
+    }
+
+    @Override
+    public void sendEmail(String to, String subject, String body) {
+
+    }
+}
