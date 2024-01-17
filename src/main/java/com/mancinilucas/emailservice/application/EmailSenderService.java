5572c59968502f3eeac2b8edd57a9e5d3aec3ed6
mancinilucas <mancini.lucasg@gmail.com>
2024-01-17 20:19:22 -0300

    add amazon ses config

--- src/main/java/com/mancinilucas/emailservice/application/EmailSenderService.java
+++ src/main/java/com/mancinilucas/emailservice/application/EmailSenderService.java
@@ -3,7 +3,9 @@ package com.mancinilucas.emailservice.application;
 import com.mancinilucas.emailservice.adapters.EmailSenderGateway;
 import com.mancinilucas.emailservice.core.EmailSenderUseCase;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class EmailSenderService implements EmailSenderUseCase {
 
     private final EmailSenderGateway emailSenderGateway;
---
+++ src/main/java/com/mancinilucas/emailservice/infrastructure/ses/SesEmailSender.java
@@ -0,0 +1,35 @@
+package com.mancinilucas.emailservice.infrastructure.ses;
+
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
+import com.amazonaws.services.simpleemail.model.*;
+import com.mancinilucas.emailservice.adapters.EmailSenderGateway;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SesEmailSender implements EmailSenderGateway {
+
+    private final AmazonSimpleEmailService amazonSimpleEmailService;
+
+    @Autowired
+    public SesEmailSender(AmazonSimpleEmailService amazonSimpleEmailService){
+        this.amazonSimpleEmailService = amazonSimpleEmailService;
+    }
+
+    @Override
+    public void sendEmail(String to, String subject, String body) {
+        SendEmailRequest request = new SendEmailRequest()
+                .withSource("mancini.lucasg@gmail.com")
+                .withDestination(new Destination().withToAddresses(to))
+                .withMessage(new Message()
+                        .withSubject(new Content(subject))
+                        .withBody(new Body().withText(new Content(body)))
+                );
+        try{
+            this.amazonSimpleEmailService.sendEmail(request);
+        } catch (AmazonServiceException e){
+            throw new EmailServiceException("Failure while sending email");
+        }
+    }
+}
